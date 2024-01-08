c95727d505a98965fcd2f96516459d4d93dd3723
defryheryanto <defry.heryanto@hotmail.com>
2024-01-08 23:59:33 +0700

    improve bson converter

--- pkg/mongoconverter/converter.go
+++ pkg/mongoconverter/converter.go
@@ -5,7 +5,11 @@ import "go.mongodb.org/mongo-driver/bson"
 func BsonDToMap(d bson.D) map[string]any {
 	m := map[string]any{}
 	for _, elem := range d {
-		m[elem.Key] = elem.Value
+		if bsonElem, ok := elem.Value.(bson.D); ok {
+			m[elem.Key] = BsonDToMap(bsonElem)
+		} else {
+			m[elem.Key] = elem.Value
+		}
 	}
 	return m
 }

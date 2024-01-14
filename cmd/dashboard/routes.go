f4a63c16cd3ab1adf50eeeb6a8bb704faf7f4d37
defryheryanto <defry.heryanto@hotmail.com>
2024-01-14 21:33:31 +0700

    redirect to log dashboard on root path

--- cmd/dashboard/routes.go
+++ cmd/dashboard/routes.go
@@ -14,6 +14,9 @@ func buildRoutes(h *handlers, authService auth.Service) http.Handler {
 
 	root.Handle("/static/assets/*", http.StripPrefix("/static/assets/", http.FileServer(http.Dir(fmt.Sprintf("%s/assets", config.WebFolderPath)))))
 
+	root.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/dashboard/logs", http.StatusSeeOther)
+	})
 	root.Get("/login", h.AuthHandler.LoginView)
 	root.Post("/login/action", h.AuthHandler.LoginAction)
 

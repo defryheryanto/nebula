bc0422b37c76e42a131ec61365392dd7bebd03d5
defryheryanto <defry.heryanto@hotmail.com>
2024-01-14 22:14:08 +0700

    redirect to logs path when authorized

--- cmd/dashboard/routes.go
+++ cmd/dashboard/routes.go
@@ -14,11 +14,14 @@ func buildRoutes(h *handlers, authService auth.Service) http.Handler {
 
 	root.Handle("/static/assets/*", http.StripPrefix("/static/assets/", http.FileServer(http.Dir(fmt.Sprintf("%s/assets", config.WebFolderPath)))))
 
-	root.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/dashboard/logs", http.StatusSeeOther)
+	root.Group(func(r chi.Router) {
+		r.Use(auth.RedirectAuthorizedMiddleware(authService, "/dashboard/logs"))
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "/dashboard/logs", http.StatusSeeOther)
+		})
+		r.Get("/login", h.AuthHandler.LoginView)
+		r.Post("/login/action", h.AuthHandler.LoginAction)
 	})
-	root.Get("/login", h.AuthHandler.LoginView)
-	root.Post("/login/action", h.AuthHandler.LoginAction)
 
 	root.Group(func(r chi.Router) {
 		r.Post("/api/logs", h.LogAPIHandler.CreateLog)
--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -21,3 +21,23 @@ func CheckTokenMiddleware(authService Service) func(next http.Handler) http.Hand
 		})
 	}
 }
+
+func RedirectAuthorizedMiddleware(authService Service, redirectTo string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			cookie, err := r.Cookie("access_token")
+			if err != nil {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			_, err = authService.ValidateToken(r.Context(), cookie.Value)
+			if err != nil {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			http.Redirect(w, r, redirectTo, http.StatusSeeOther)
+		})
+	}
+}
